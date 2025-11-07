e4898413943948e381700e95baa2288b0449e467
yoorrll <lyl1567@naver.com>
2025-11-07 16:41:01 +0900

    수정

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 export const corsMiddleware = cors({
   origin: [
     'http://localhost:3000',
-    'https://9-forestofstudy-1team.netlify.app/',
+    'https://9-forestofstudy-1team.netlify.app',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
