721f6581056000ce8e236958eb73c28867afcb13
yoorrll <lyl1567@naver.com>
2025-11-07 16:36:17 +0900

    cors 설정

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,7 +1,10 @@
 import cors from 'cors';
 
 export const corsMiddleware = cors({
-  origin: 'http://localhost:3000', 
+  origin: [
+    'http://localhost:3000',
+    'https://nine-forestofstudy-1team-be.onrender.com',
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 });
